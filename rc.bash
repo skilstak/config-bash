1342e6750e40b6d6ee7579d9946357ad092440a5
robmuh <rwx@robs.io>
2019-06-27 09:58:09 -0400

    dircolors added

--- rc.bash
+++ rc.bash
@@ -55,3 +55,10 @@ fi
 export EDITOR=vi
 export VISUAL=vi
 
+if [ -x /usr/bin/dircolors ]; then
+  if [ -r ~/.dircolors ]; then
+    eval "$(dircolors -b ~/.dircolors)"
+  else
+    eval "$(dircolors -b)"
+  fi
+fi
