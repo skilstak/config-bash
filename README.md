2f9406b89366668bd10c8774a03170abb3bc7e6f
robmuh <rwx@robs.io>
2019-07-03 20:30:30 -0400

    save

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Basic SkilStak Bash Configuration
 
 Used in the lab and to get new Linux users started right.
+
+Uses `$HOME/repos/config` for most things.
--- aliases.bash
+++ aliases.bash
@@ -39,3 +39,4 @@ alias ghping="ssh git@github.com"
 alias bat='upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state|to\ full|percentage"'
 alias vimpluginstall="vim +':PlugInstall' +':q!' +':q!'"
 alias open="gnome-open"
+alias config="cd $HOME/repos/config"
